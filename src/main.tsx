bb12150a0e31ceebccd4d6206d3a788733c1e378
shixinwang-create <shixinwang0306@gmail.com>
2026-04-27 17:23:20 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css"; // 👈 這行就是關鍵
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
